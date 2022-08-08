aca3f4813d3fecd07795148a6a6408cf7ae35c17
ravigadhavi2 <104010012+ravigadhavi2@users.noreply.github.com>
2022-08-08 09:14:44 +0530

    day 2

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(
+        child: Text("Welcome to $days days of flutter"),
+      )
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
