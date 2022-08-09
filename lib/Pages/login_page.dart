10acec81c586222b1fb0fc249663f484bdd90c50
ravigadhavi2 <104010012+ravigadhavi2@users.noreply.github.com>
2022-08-09 14:10:05 +0530

    navigate from one page to another

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,57 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/Pages/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Username",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      print("Login");
-                    },
-                    child: Text("Login"),
-                    style: TextButton.styleFrom())
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AppRoutes.homeRoute);
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(150,40)
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled1/Pages/home_page.dart';
 import 'package:untitled1/Pages/login_page.dart';
+import 'package:untitled1/Pages/utils/routes.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,15 +13,16 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     int days =30;
       return MaterialApp(
+        debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(brightness: Brightness.dark,
       fontFamily: GoogleFonts.lato().fontFamily,
       primaryTextTheme: GoogleFonts.latoTextTheme()),
-      initialRoute: "/home",
+      initialRoute: "/",
           routes:{
           "/":(context)=> LoginPage(),
-            "/home":(context)=>LoginPage(),
-        "/login":(context)=>LoginPage(),
+            AppRoutes.homeRoute:(context)=>HomePage(),
+            AppRoutes.loginRoute:(context)=>LoginPage(),
       });
   }
   
